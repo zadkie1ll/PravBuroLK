ee3292c4562951165bd2c207d1041d8dffc4c474
AndreyK161 <moonandre9999@mail.ru>
2026-08-05 11:41:19 +0300

    feat: внедрения urlshorter в прод

--- services/urlshorter_service/backend/app/main.py
+++ services/urlshorter_service/backend/app/main.py
@@ -7,7 +7,7 @@ app = FastAPI(title="URL Shortener Service", version="0.1.0")
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["http://localhost:5179", "http://127.0.0.1:5179"],
+    allow_origins=["http://localhost:5179", "http://127.0.0.1:5179", "https://panel.prav-buro.ru"],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
