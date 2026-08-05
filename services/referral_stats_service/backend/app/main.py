9ed9bf93306107b624885c030c8aa1e36d01a16e
AndreyK161 <moonandre9999@mail.ru>
2026-08-05 16:22:06 +0300

    feat: подготовка к проду referral_stats

--- services/referral_stats_service/backend/app/main.py
+++ services/referral_stats_service/backend/app/main.py
@@ -7,7 +7,7 @@ app = FastAPI(title="Referral Stats Service", version="0.1.0")
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["http://localhost:5178", "http://127.0.0.1:5178"],
+    allow_origins=["http://localhost:5178", "http://127.0.0.1:5178", "https://panel.prav-buro.ru"],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
