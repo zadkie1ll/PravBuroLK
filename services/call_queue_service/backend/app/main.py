2b2cd249afd1e20f4d0810394ca3cf10f593499d
AndreyK161 <moonandre9999@mail.ru>
2026-08-07 11:12:44 +0300

    feat: сервис call_queue

--- services/call_queue_service/backend/app/main.py
+++ services/call_queue_service/backend/app/main.py
@@ -7,7 +7,7 @@ app = FastAPI(title="Call Queue Service", version="0.1.0")
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["http://localhost:5173", "http://127.0.0.1:5173"],
+    allow_origins=["http://localhost:5173", "http://127.0.0.1:5173", "https://panel.prav-buro.ru"],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
